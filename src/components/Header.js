4295298626aaaa3661da42ebc240e3314b7425de
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-08-08 20:05:58 -0400

    made landing page/header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Navlink} from 'react-router-dom'
+
+function Header () {
+    return (
+        <header>
+            <nav>
+                <Navlink to="/">Home</Navlink>
+            </nav>
+
+
+        </header>
+    )
+}
