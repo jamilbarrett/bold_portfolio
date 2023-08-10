1205e2ac36cbf6e4a4bc79e598406084c10b1e41
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-08-09 21:58:21 -0400

    added bg and resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
-        <>
-        </>
+        <footer className="footer">
+            <a href="https://github.com/jamilbarrett"><FontAwesomeIcon icon={faGithub} />
+            </a>
+       </footer>
     )
 }
 
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,38 @@
-function Resume () {
+function Resume() {
     return (
         <>
-        <h1>The Works</h1>
-        
-        
+            <div className="resumeContainer">
+            <h1 className="centerh1">Resume</h1>
+            <section id="experience">
+                <h3>Work Experience</h3>
+                <p>Keller Williams
+                    <ul>
+                        <li>Achieved $18.9 million in gross sales earning, ‘Rookie of the Year’ and ‘Agent of the Month’</li>
+                        <li>Prepares representation contracts, exclusive right to sell agreements and residential/commercial leases</li>
+                        <li>Processes lease applications, which includes qualifying prospects and handing credit verifications</li>
+                        <li>Assists buyers & sellers with the purchase and sale of properties from conceptualization to implementation</li>
+                    </ul>
+                </p>
+                <p>Tech Briefs Media Group
+                    <ul>
+                        <li>Aggregated and translated details of analytic data on subscriber information upwards of 1 million customers to
+                            support a large network of business partners across the Tech Briefs Media Group</li>
+                        <li>Deployed e-newsletters, digital magazines, and email campaigns, including delivery and CTR metric reports</li>
+                        <li>Led monthly meetings to discuss the database best practices, managing workload, and to get a general sense of the
+                            team’s ability to operate</li>
+                        <li>Created behavioral profiles by analyzing consumer data</li>
+                        <li>Created managed, and optimized list segments across disparate databases and email list servers, including de-duping
+                            of outside lists with the principal database</li>
+                        <li>Supervised a team of 2 digital marketing associates</li>
+                    </ul>
+                </p>
+            </section>
+            <section id="skills">
+
+            </section>
+
+
+        </div >
         </>
     )
 }
