328d9c8311fa2fc4e0c32312990c3d75c54357d4
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-08-15 20:12:50 -0400

    addedstyleport

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,52 +1,63 @@
 import React from 'react';
+import './Portfolio.css'; // Assuming you have a separate CSS file for styling
 
 function Portfolio() {
   return (
     <>
       <section className="text-white">
-      <div className="border border-white rounded-lg">
+        <div className="border border-white rounded-lg">
 
-        <h2 className="text-3xl p-4 font-semibold mb-4">The Collection...</h2>
-        <div className="mb-4">
-          <a href="https://jamilbarrett.github.io/work_daze/" target="_blank" rel="noopener noreferrer">
+          <h2 className="text-3xl p-4 font-semibold mb-4">The Collection...</h2>
+          <div className="mb-4">
+            <a href="https://jamilbarrett.github.io/work_daze/" target="_blank" rel="noopener noreferrer">
 
-          <img
-            src="wkday.png"
-            alt="workday"
-            className="w-full h-40 object-cover rounded-lg"
-          />
-          </a>
+              <div className="image-container">
+                <img
+                  src="wkday.png"
+                  alt="workday"
+                  className="w-full h-40 object-cover rounded-lg"
+                />
+                <div className="image-title">Work Day Scheduler</div>
+              </div>
+            </a>
           </div>
         </div>
       </section>
       <section className="flex flex-col space-y-4">
         <div className="border border-white rounded-lg">
           <a href="https://moodsync-calendar-1a40e0647873.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <div className='image-container'>
-            <img
-              src="Moodsync.png"
-              alt="Project 2"
-              className="w-full h-40 object-cover rounded-lg"
-            />
+            <div className="image-container">
+              <img
+                src="Moodsync.png"
+                alt="Project 2"
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <div className="image-title">Moodsync Calendar</div>
             </div>
           </a>
         </div>
         <div className="border border-white rounded-lg">
           <a href="https://jamilbarrett.github.io/weather-flix-project/" target="_blank" rel="noopener noreferrer">
-            <img
-              src="WeatherFlix.png"
-              alt="WeatherFlix"
-              className="w-full h-40 object-cover rounded-lg"
-            />
+            <div className="image-container">
+              <img
+                src="WeatherFlix.png"
+                alt="WeatherFlix"
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <div className="image-title">WeatherFlix</div>
+            </div>
           </a>
         </div>
         <div className="border border-white rounded-lg">
           <a href="https://github.com/jamilbarrett/ecomm_backend" target="_blank" rel="noopener noreferrer">
-            <img
-              src="ecommback.png"
-              alt="Ecommback"
-              className="w-full h-40 object-cover rounded-lg"
-            />
+            <div className="image-container">
+              <img
+                src="ecommback.png"
+                alt="Ecommback"
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <div className="image-title">Ecomm Backend</div>
+            </div>
           </a>
         </div>
       </section>
